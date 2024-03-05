7a2938a9c9c91c5b5cecbf68eee596f11d37b66f
aleksandrblazheiko <blazheiko@gmail.com>
2024-03-05 21:36:49 +0200

    add pino-pretty

--- logger.js
+++ logger.js
@@ -1,5 +1,13 @@
 import pino from 'pino';
+import appConfig from '#config/app.js';
 
-const logger = pino();
+const logger =
+    appConfig.env === 'prod' || appConfig.env === 'production'
+        ? pino()
+        : pino({
+              transport: {
+                  target: 'pino-pretty',
+              },
+          });
 
 export default logger;
